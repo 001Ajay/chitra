33fa20c7ec9f0974cf31dcc621626c79916d731a
Ajay Das <ajay4.das4@cognizant.com>
2017-04-21 11:57:42 +0530

    JSP Tiles view configured
    View Resolver configured to use tiles

--- src/main/java/chitra/web/controller/BaseController.java
+++ src/main/java/chitra/web/controller/BaseController.java
@@ -1,14 +1,31 @@
 package chitra.web.controller;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import chitra.web.form.ContactForm;
 
 @RestController
+@RequestMapping("/base")
 public class BaseController {
 	
 	@RequestMapping("/check")
 	public String checkString(){
 		return "I am ready";
 	}
+	
+	@RequestMapping("/contact")
+	public ModelAndView contact(){
+		ModelAndView model = new ModelAndView("check","command",new ContactForm());		
+		return model;
+	}
 
+	@RequestMapping(value = "/addContact", method = RequestMethod.POST)  
+    public String addContact(@ModelAttribute("contact") ContactForm contact) {  
+        //write the code here to add contact  
+        return "sucess";  
+    }
 }
---
+++ src/main/java/chitra/web/controller/ChitraModel.java
@@ -0,0 +1,8 @@
+package chitra.web.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public class ChitraModel extends ModelAndView{
+
+	
+}
